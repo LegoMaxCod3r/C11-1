8bb618abac914083ce78bf8eac7a0d6f77cb94ec
LegoMaxCod3r <93746195+LegoMaxCod3r@users.noreply.github.com>
2021-11-09 18:52:40 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,173 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+var cloud, cloudImage;
+var obstacle1, obstacle2,obstacle3,obstacle4,obstacle5,obstacle6;
+
+
+
+
+var score=0;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+ 
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+
+
+}
+
+function setup() {
+
+  createCanvas(600,200)
+  
+  //crear sprite de trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  //crear sprite de suelo
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  console.log("hello"+"World");
+
+
+  //crear sprite de suelo invisible
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //generar números aleatorios
+  var rand =  Math.round(random(1,100))
+  //console.log(rand)
+
+}
+
+function draw() {
+  //establecer color de fondo
+  background(180);
+  
+  text("score: " + score, 500, 50)
+  score = Math.round(frameCount/5);
+  //console.log(trex.y)
+  
+  
+  
+  //hacer que el trex salte al presionar la barra espaciadora
+  if(keyDown("space")&& trex.y >= 160) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //evitar que el trex caiga
+  trex.collide(invisibleGround);
+  
+  //aparecer nubes
+  spawnClouds();
+
+  spawnObstacles();
+  
+  drawSprites();
+}
+
+function spawnObstacles() {
+  var obstacle;
+  if(frameCount%60===0){
+
+    obstacle = createSprite(600,160,10,40);
+    obstacle.velocityX= -6;
+  
+    var rand = Math.round(random(1,6));
+
+    switch(rand){
+
+      case 1:
+        obstacle.addImage(obstacle1);
+        break;
+
+      case 2:
+        obstacle.addImage(obstacle2);
+        break;
+
+      case 3:
+         obstacle.addImage(obstacle3);
+         break;
+
+      case 4:
+          obstacle.addImage(obstacle4);
+          break;
+
+      case 5:
+         obstacle.addImage(obstacle5);
+         break;
+   
+      case 6:
+        obstacle.addImage(obstacle6);
+         break;
+
+
+    }
+
+    
+    obstacle.scale=0.5;
+    
+    obstacle.depth=trex.depth;
+    trex.depth=trex.depth+1;
+  
+    obstacle.lifetime=300;
+
+  }
+
+}
+
+
+
+//función para aparecer las nubes
+function spawnClouds(){
+ //escribir aquí el código 
+ 
+
+
+ if(frameCount%60===0){
+  //console.log(frameCount);
+
+
+  cloud = createSprite(600,100,40,10);
+  cloud.velocityX= -3;
+
+  cloud.addImage(cloudImage);
+  cloud.scale=Math.random(0.5,1);
+  cloud.y=Math.round(random(10,100));
+  cloud.depth=trex.depth;
+  trex.depth=trex.depth+1;
+
+  cloud.lifetime=200;
+
+  console.log(trex.depth);
+  console.log(cloud.depth);
+
+ }
+
+
+}
+
+
+
+
